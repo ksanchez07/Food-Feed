6ab1a32e9a86dbd8d96182df51b70d986926bdd3
Ellie <ellier621@gmail.com>
2025-10-04 21:34:51 -0500

    Initial Commit

---
+++ FoodFeed/FoodFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFeedApp.swift
+//  FoodFeed
+//
+//  Created by Ellie on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
